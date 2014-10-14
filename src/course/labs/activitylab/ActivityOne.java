cf79ec8fed02c085767d25634e83cfbb32eaf69b
Lucas Silvestre <lukas.silvestre@gmail.com>
2014-10-14 17:28:15 -0300

    Implements the OnClickListener for the launchActivityTwoButton

--- src/course/labs/activitylab/ActivityOne.java
+++ src/course/labs/activitylab/ActivityOne.java
@@ -67,10 +67,10 @@ public class ActivityOne extends Activity {
 
 				// Create an intent stating which Activity you would like to
 				// start
-				Intent intent = null;
+				Intent intent = new Intent(ActivityOne.this, ActivityTwo.class);
 
 				// Launch the Activity using the intent
-
+				startActivity(intent);
 			}
 		});
 
